d28bee017258cefde28b40b92ddb20a073a2db4a
Artyom Borisov <2001raf@gmail.com>
2025-10-26 15:00:23 +0500

    Update valid in sequenc_loss

--- MonSter++/train_us3d.py
+++ MonSter++/train_us3d.py
@@ -73,7 +73,7 @@ def sequence_loss(disp_preds, disp_init_pred, disp_gt, valid, loss_gamma=0.9, ma
     assert n_predictions >= 1
     disp_loss = 0.0
     mag = torch.sum(disp_gt**2, dim=1).sqrt()
-    valid = ((valid >= 0.5) & (disp_gt > 0) & (mag < max_disp)).unsqueeze(1)
+    valid = ((valid >= 0.5) & (mag < max_disp)).unsqueeze(1) & (disp_gt > 0)
     assert valid.shape == disp_gt.shape, [valid.shape, disp_gt.shape]
     assert not torch.isinf(disp_gt[valid.bool()]).any()
 
