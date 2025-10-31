95a4e05b43c025a5118d6f467f3ca030ebe1b0ad
Artyom Borisov <2001raf@gmail.com>
2025-11-01 00:16:10 +0500

    Use different split ratios for different cities

--- MonSter++/split_us3d.py
+++ MonSter++/split_us3d.py
@@ -15,9 +15,12 @@ DEST_TRAIN_DIR = os.path.join(DEST_ROOT, "train")
 DEST_VAL_DIR = os.path.join(DEST_ROOT, "val")
 DEST_TEST_DIR = os.path.join(DEST_ROOT, "test")
 SEED = 666
-TRAIN_RATIO = 0.8
-VAL_RATIO = 0.1
-TEST_RATIO = 0.1
+JAX_TRAIN_RATIO = 0.7
+JAX_VAL_RATIO = 0.1
+JAX_TEST_RATIO = 0.2
+OMA_TRAIN_RATIO = 0.0
+OMA_VAL_RATIO = 0.0
+OMA_TEST_RATIO = 1.0
 TRAIN_CROP_SIZE = (256, 1024)
 SPLIT_INFO_BASENAME = "split_info.txt"
 
@@ -44,38 +47,19 @@ def main():
     print(f"Dest val dir: {DEST_VAL_DIR}")
     print(f"Dest test dir: {DEST_TEST_DIR}")
     print(f"Seed: {SEED}")
-    print(f"Train ratio: {TRAIN_RATIO}")
-    print(f"Val ratio: {VAL_RATIO}")
-    print(f"Test ratio: {TEST_RATIO}")
+    print(f"JAX train ratio: {JAX_TRAIN_RATIO}")
+    print(f"JAX val ratio: {JAX_VAL_RATIO}")
+    print(f"JAX test ratio: {JAX_TEST_RATIO}")
+    print(f"OMA train ratio: {OMA_TRAIN_RATIO}")
+    print(f"OMA val ratio: {OMA_VAL_RATIO}")
+    print(f"OMA test ratio: {OMA_TEST_RATIO}")
     print(f"Train crop size: {TRAIN_CROP_SIZE}")
     print("--------------------------------")
-    print("Setting seed...")
-    np.random.seed(SEED)
-    random.seed(SEED)
-    print("Done")
     print("Checking if source directories exist...")
     assert os.path.exists(SOURCE_IMAGES_ROOT)
     assert os.path.exists(SOURCE_DISP_ROOT)
     print("Done")
     print("Checking if destination directories exist...")
-    if os.path.exists(DEST_TRAIN_DIR):
-        answer = input("Dest train dir already exists, do you want to continue? (y/n) ")
-        if str.lower(answer) != "y":
-            print("Exiting...")
-            exit(0)
-        shutil.rmtree(DEST_TRAIN_DIR)
-    if os.path.exists(DEST_VAL_DIR):
-        answer = input("Dest val dir already exists, do you want to continue? (y/n) ")
-        if str.lower(answer) != "y":
-            print("Exiting...")
-            exit(0)
-        shutil.rmtree(DEST_VAL_DIR)
-    if os.path.exists(DEST_TEST_DIR):
-        answer = input("Dest test dir already exists, do you want to continue? (y/n) ")
-        if str.lower(answer) != "y":
-            print("Exiting...")
-            exit(0)
-        shutil.rmtree(DEST_TEST_DIR)
     print("Creating destination directories...")
     os.makedirs(DEST_TRAIN_DIR, exist_ok=True)
     os.makedirs(DEST_VAL_DIR, exist_ok=True)
@@ -89,14 +73,19 @@ def main():
     print(f"Found {len(image_left_list)} images")
     print("Splitting data into train, val, and test sets...")
     data_list = list(zip(image_left_list, image_right_list, disp_list))
-    train_list = list(filter(lambda x: x[0].split("/")[-1].startswith("JAX"), data_list))
-    val_list = list(filter(lambda x: x[0].split("/")[-1].startswith("OMA"), data_list))
-    test_list = []
+    jax_train_list, jax_val_list, jax_test_list = random_split(data_list, [JAX_TRAIN_RATIO, JAX_VAL_RATIO, JAX_TEST_RATIO])
+    oma_train_list, oma_val_list, oma_test_list = random_split(data_list, [OMA_TRAIN_RATIO, OMA_VAL_RATIO, OMA_TEST_RATIO])
+    train_list = jax_train_list + oma_train_list
+    val_list = jax_val_list + oma_val_list
+    test_list = jax_test_list + oma_test_list
     print("Done")
     print("--------------------------------")
-    print(f"Train split: {TRAIN_RATIO}, {len(train_list)} train images")
-    print(f"Val split: {VAL_RATIO}, {len(val_list)} val images")
-    print(f"Test split: {TEST_RATIO}, {len(test_list)} test images")
+    print(f"JAX train split: {JAX_TRAIN_RATIO}, {len(jax_train_list)} JAX train images")
+    print(f"JAX val split: {JAX_VAL_RATIO}, {len(jax_val_list)} JAX val images")
+    print(f"JAX test split: {JAX_TEST_RATIO}, {len(jax_test_list)} JAX test images")
+    print(f"OMA train split: {OMA_TRAIN_RATIO}, {len(oma_train_list)} OMA train images")
+    print(f"OMA val split: {OMA_VAL_RATIO}, {len(oma_val_list)} OMA val images")
+    print(f"OMA test split: {OMA_TEST_RATIO}, {len(oma_test_list)} OMA test images")
     print("--------------------------------")
     print("Cropping the train images and saving to the train directory...")
     for (image_left_path, image_right_path, disp_path) in tqdm(train_list, desc="train", unit="image"):
@@ -129,11 +118,17 @@ def main():
     print("Done")
     print("Saving the split information to the split info file...")
     with open(os.path.join(DEST_ROOT, SPLIT_INFO_BASENAME), "w") as f:
-        f.write(f"Train ratio: {TRAIN_RATIO}\n")
-        f.write(f"Val ratio: {VAL_RATIO}\n")
-        f.write(f"Test ratio: {TEST_RATIO}\n")
-        f.write(f"Train crop size: {TRAIN_CROP_SIZE}\n")
+        f.write(f"JAX train ratio: {JAX_TRAIN_RATIO}\n")
+        f.write(f"JAX val ratio: {JAX_VAL_RATIO}\n")
+        f.write(f"JAX test ratio: {JAX_TEST_RATIO}\n")
+        f.write(f"OMA train ratio: {OMA_TRAIN_RATIO}\n")
+        f.write(f"OMA val ratio: {OMA_VAL_RATIO}\n")
+        f.write(f"OMA test ratio: {OMA_TEST_RATIO}\n")
+        f.write(f"Train list length: {len(train_list)}\n")
+        f.write(f"Val list length: {len(val_list)}\n")
+        f.write(f"Test list length: {len(test_list)}\n")
         f.write(f"Seed: {SEED}\n")
+        f.write(f"Train crop size: {TRAIN_CROP_SIZE}\n")
     print("Done")
     print("--------------------------------")
     print("The US3D dataset has been split successfully")
