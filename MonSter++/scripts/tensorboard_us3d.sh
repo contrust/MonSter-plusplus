dc380cd0a494950171c2b30a46d3b99b5f71cddf
Artyom Borisov <2001raf@gmail.com>
2026-02-17 02:15:31 +0500

    Update logdir for tensorboard script

--- MonSter++/scripts/tensorboard_us3d.sh
+++ MonSter++/scripts/tensorboard_us3d.sh
@@ -2,5 +2,5 @@
 
 eval "$(conda shell.bash hook)"
 conda activate monster-plus-plus
-tensorboard --logdir ./logs/flip/ --samples_per_plugin images=0
+tensorboard --logdir ./logs/us3d/ --samples_per_plugin images=0
 #tensorboard --logdir ./logs/us3d/
